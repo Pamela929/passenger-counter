31f83b0609829a89fbe80462bc97879bcc5daabe
Pamela Hartwig <pamela929@gmail.com>
2023-07-05 13:49:49 +0200

    my dog age

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count);
+
+let myAge = 40
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge);
