1ca3a143366c35c82f3f8677188c2f986198bab8
Pamela Hartwig <pamela929@gmail.com>
2023-07-06 13:26:14 +0200

    Finished the counter app

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-
-console.log(countEl);
 let count = 0
 
 function increment() {
-  count = count + 1
-  countEl.innerText = count
+  count += 1
+  countEl.textContent = count
 }
 
 function save() {
-  console.log(count);
+let countStr = count + " - "
+saveEl.textContent += countStr
+countEl.textContent = 0
+count = 0
 }
+
